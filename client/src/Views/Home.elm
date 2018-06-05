8d4ff7e62ad3b6b5973bd88c52ff2e3a7c20dd9e
mantagen <mathsgregory@gmail.com>
2018-06-05 11:33:04 +0100

    Adjust query inputs styling #20

--- client/src/Views/Home.elm
+++ client/src/Views/Home.elm
@@ -9,19 +9,54 @@ import Html.Events exposing (onSubmit, onInput)
 view : Model -> Html Msg
 view model =
     div [ class "flex justify-center" ]
-        [ div [ class "relative w-60 bg-white pa3 br-pill ba b--light-gray" ] [ queryForm ]
+        [ div [ class "relative w-60 bg-white pa3 br-pill ba b--light-gray" ]
+            [ div [ class "pr3" ] [ queryForm ]
+            ]
         ]
 
 
 queryForm : Html Msg
 queryForm =
     form [ onSubmit SubmitLoginEmailForm ]
-        [ input [ class "mh1 pv2 ph4 br-pill b--solid b--blue", type_ "text", placeholder "Type your country", onInput LoginEmailFormOnInput ] []
-        , span [ class "br b--light-gray w1 mr3 pa2" ] []
-        , input [ class "mh1 pv2 ph4 br-pill b--solid b--blue", type_ "text", placeholder "Activity", onInput LoginEmailFormOnInput ] []
-        , span [ class "br b--light-gray w1 mr3 pa2" ] []
-        , input [ class "mh1 pv2 ph4 br-pill b--solid b--blue", type_ "text", placeholder "Catergory", onInput LoginEmailFormOnInput ] []
-        , div [ class "di pa3 br-100 bg-blue" ]
-            [ button [ class "bg-blue white f5 metro bn", value "submit" ] [ text "Go" ]
+        [ input
+            [ class inputClass
+            , type_ "text"
+            , placeholder "Type your country"
+            , onInput LoginEmailFormOnInput
             ]
+            []
+        , divider
+        , input
+            [ class inputClass
+            , type_ "text"
+            , placeholder "Activity"
+            , onInput LoginEmailFormOnInput
+            ]
+            []
+        , divider
+        , input
+            [ class inputClass
+            , type_ "text"
+            , placeholder "Catergory"
+            , onInput LoginEmailFormOnInput
+            ]
+            []
+        , submitButton
+        ]
+
+
+divider : Html msg
+divider =
+    div [ class "w-05 fl h2 flex justify-center" ] [ div [ class "br b--light-gray w0 h2 mh0" ] [] ]
+
+
+inputClass : String
+inputClass =
+    "w-30 h2 fl pv2 ph3 br-pill ba b--solid b--blue"
+
+
+submitButton : Html msg
+submitButton =
+    div [ class "di pa2 absolute right--2 top-0 h3 w3" ]
+        [ button [ class "br-100 bg-blue white f5 metro bn h-100 w-100", value "submit" ] [ text "Go" ]
         ]
