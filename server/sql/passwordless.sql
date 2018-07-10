d74bd13b512cfb8bc09d9d423dbf4af3b0d33de0
jbarget <dear.justen@gmail.com>
2018-07-10 16:38:17 +0100

    updating created_at timestamp when passwordless row is updated #115

--- server/sql/passwordless.sql
+++ server/sql/passwordless.sql
@@ -10,3 +10,16 @@ CREATE TABLE passwordless
   code character (4) NOT NULL,
   created_at timestamp not null default now()
 );
+
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.created_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON passwordless
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
