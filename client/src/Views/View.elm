55c5da15510b979fdcf87df39e8dc872a57c63bf
jbarget <dear.justen@gmail.com>
2018-07-16 15:48:31 +0200

    adding fixed contact banner across all pages #132

--- client/src/Views/View.elm
+++ client/src/Views/View.elm
@@ -7,12 +7,14 @@ import Util exposing (viewIf)
 import Model exposing (Model, Msg(..))
 import Views.Header as Header
 import Views.Footer as Footer
+import ContactBanner
 
 
 view : Model -> Html Msg
 view model =
     div [ class "bg-near-white min-vh-100 flex flex-column justify-between" ]
-        [ viewIf (not (String.contains "query" model.location.hash)) (Header.view model)
+        [ ContactBanner.view
+        , viewIf (not (String.contains "query" model.location.hash)) (Header.view model)
         , matchView model
         , Footer.view model
         ]
